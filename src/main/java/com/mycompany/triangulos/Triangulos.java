cdf7cc1e15b36369c22b65e66ca87a69bf06162d
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-10-05 17:54:13 -0300

    Update Triangulos.java

--- src/main/java/com/mycompany/triangulos/Triangulos.java
+++ src/main/java/com/mycompany/triangulos/Triangulos.java
@@ -5,17 +5,18 @@ public class Triangulos {
 
     public static void main(String[] args) {
         Triangulos2 lados = new Triangulos2();
-        Scanner sides = new Scanner(System.in);
         
-        
+        Scanner side1 = new Scanner(System.in);
         System.out.println("Digite o primeiro lado:");
-        lados.lado1 = (sides.nextFloat());
+        lados.lado1 = (side1.nextDouble());
         
+        Scanner side2 = new Scanner(System.in);
         System.out.println("Digite o segundo lado:");
-        lados.lado2 = (sides.nextFloat());
+        lados.lado2 = (side2.nextDouble());
         
+        Scanner side3 = new Scanner(System.in);
         System.out.println("Digite o terceiro lado:");
-        lados.lado3 = (sides.nextFloat());
+        lados.lado3 = (side3.nextDouble());
         
         lados.saida();
     }
